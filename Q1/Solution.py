a7a8e4993908c14cad7b3002027495b39b104a78
ramsha1236 <139097483+ramsha1236@users.noreply.github.com>
2024-12-05 12:22:38 +0400

    Update Solution.py

--- Q1/Solution.py
+++ Q1/Solution.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
 
-class ACM_Debugging_Competition:
+class ACM_Debugging_Competition { #added a curly brace
     @app.route("/bye")
     def index():
         return "hi"
@@ -12,6 +12,6 @@ class ACM_Debugging_Competition:
     def bye():
         return "bye"
 
-
+}
 if __name__ == "__main__":
     app.run(debug=False)
